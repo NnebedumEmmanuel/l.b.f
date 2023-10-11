5d4304494964d6d9da129cae19871a5970734e4a
NnebedumEmmanuel <emmanuelnnebedum704@gmail.com>
2023-10-11 12:49:51 +0100

    update

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import {
+  createBrowserRouter, 
+  createRoutesFromElements,
+  Route, 
+  RouterProvider
+} from 'react-router-dom'
+
+//pages
+import { Home } from "./Pages/Home";
+import { About } from "./Pages/About";
+import { Sermons } from "./Pages/Sermons";
+import { Contact } from "./Pages/Contact";
+
+
+//Routes
+import RootLayout from "./Routes/RootLayout";
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/Sermons" element={<Sermons />} />
+      <Route path="/contact" element={<Contact />} />
+    </Route>
+  )
+)
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
