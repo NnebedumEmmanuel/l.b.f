34bccf48fd1b97bc74a5fa22399db1f8ecaefe69
NnebedumEmmanuel <emmanuelnnebedum704@gmail.com>
2023-10-17 14:10:27 +0100

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import "./Navbar.css"
 import { Navbar, Container, Nav, } from "react-bootstrap";
 import { NavLink } from 'react-router-dom';
 import Logo from "../Assets/img/lb-logo.jpg"
 function NavBar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
   return (
     <>
       <Navbar bg="success" data-bs-theme="dark">
@@ -14,9 +18,9 @@ function NavBar() {
           </Navbar.Brand>
 
           <Nav className=" d-flex  justify-content-end">
-          <li className="nav-item"><NavLink to="/" className="nav-link">Home</NavLink></li>
+          <li className="nav-item"><NavLink to="/" className="nav-link" activeClassName="active"  onClick={handleClick}>Home</NavLink></li>
             
-            <li className='nav-item'><NavLink to="Sermons" className="nav-link">Sermons</NavLink></li>
+            <li activeClassName="active" className='nav-item '><NavLink  to="Sermons" className="nav-link">Sermons</NavLink></li>
            <li className="nav-item"> <NavLink to="About" className="nav-link">About</NavLink></li>
            <li className="nav-item"> <NavLink to="Contact" className="nav-link">Contact</NavLink></li>
               
