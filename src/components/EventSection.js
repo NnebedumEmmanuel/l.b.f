5b41e41941938e7c5b7f9f49471fc9a7998a7a42
NnebedumEmmanuel <emmanuelnnebedum704@gmail.com>
2023-11-06 10:46:13 +0100

    update

---
+++ src/components/EventSection.js
@@ -0,0 +1,134 @@
+import React from 'react'
+
+const EventSection = () => {
+  return (
+   <div class="event padding--top padding--bottom">
+        <div class="container">
+            <div class="section__header text-center">
+                <h2>Recent Event</h2>
+                <p>Enthusiastically underwhelm quality benefits rather than professional outside the box thinking. Distinctively network highly efficient leadership skills</p>
+            </div>
+            <div class="section__wrapper">
+                <div class="row g-4 justify-content-center">
+                    <div class="col-lg-4 col-sm-6 col-12">
+                        <div class="event__item">
+                            <div class="event__inner">
+                                <div class="event__thumb">
+                                    <a href="event-single.html"><img src="assets/images/event/01.jpg" alt="event thumb" /></a>
+                                    <div class="event__thumb-date">
+                                        <h6>09</h6>
+                                        <p>Nov</p>
+                                    </div>
+                                </div>
+                                <div class="event__content">
+                                    <a href="event-single.html"><h5>Open Rededication</h5></a>
+                                    <div class="event__metapost">
+                                        <ul class="event__metapost-info">
+                                            <li><i class="far fa-clock"></i> 10am - 12pm</li>
+                                            <li><i class="fas fa-map-marker-alt"></i> PO Box 16122, Collins Street</li>
+                                        </ul>
+                                        <ul class="event__metapost-comentshare">
+                                            <li class="event__metapost-coment">
+                                                <i class="far fa-comments"></i>
+                                                <a href="#" class="event__metapost-count">10</a>
+                                            </li>
+                                            <li class="event__metapost-share">
+                                                <i class="fas fa-share-alt"></i>
+                                                <ul>
+                                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                                    <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore consectetur adipisicing elit</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 col-12">
+                        <div class="event__item">
+                            <div class="event__inner">
+                                <div class="event__thumb">
+                                    <a href="event-single.html"><img src="assets/images/event/02.jpg" alt="event thumb" /></a>
+                                    <div class="event__thumb-date">
+                                        <h6>09</h6>
+                                        <p>Nov</p>
+                                    </div>
+                                </div>
+                                <div class="event__content">
+                                    <a href="event-single.html"><h5>Big Event This Year</h5></a>
+                                    <div class="event__metapost">
+                                        <ul class="event__metapost-info">
+                                            <li><i class="far fa-clock"></i> 10am - 12pm</li>
+                                            <li><i class="fas fa-map-marker-alt"></i> PO Box 16122, Collins Street</li>
+                                        </ul>
+                                        <ul class="event__metapost-comentshare">
+                                            <li class="event__metapost-coment">
+                                                <i class="far fa-comments"></i>
+                                                <a href="#" class="event__metapost-count">10</a>
+                                            </li>
+                                            <li class="event__metapost-share">
+                                                <i class="fas fa-share-alt"></i>
+                                                <ul>
+                                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                                    <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore consectetur adipisicing elit</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 col-12">
+                        <div class="event__item">
+                            <div class="event__inner">
+                                <div class="event__thumb">
+                                    <a href="event-single.html"><img src="assets/images/event/03.jpg" alt="event thumb" /></a>
+                                    <div class="event__thumb-date">
+                                        <h6>09</h6>
+                                        <p>Nov</p>
+                                    </div>
+                                </div>
+                                <div class="event__content">
+                                    <a href="event-single.html"><h5>Church Evert</h5></a>
+                                    <div class="event__metapost">
+                                        <ul class="event__metapost-info">
+                                            <li><i class="far fa-clock"></i> 10am - 12pm</li>
+                                            <li><i class="fas fa-map-marker-alt"></i> PO Box 16122, Collins Street</li>
+                                        </ul>
+                                        <ul class="event__metapost-comentshare">
+                                            <li class="event__metapost-coment">
+                                                <i class="far fa-comments"></i>
+                                                <a href="#" class="event__metapost-count">10</a>
+                                            </li>
+                                            <li class="event__metapost-share">
+                                                <i class="fas fa-share-alt"></i>
+                                                <ul>
+                                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                                    <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore consectetur adipisicing elit</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default EventSection
---
+++ src/components/ScheduleSection.js
@@ -0,0 +1,269 @@
+import React from 'react'
+
+const ScheduleSection = () => {
+  return (
+    <div class="shedule padding--top padding--bottom bg-img" style="background-image: url(assets/images/bg-img/01.jpg);">
+        <div class="container">
+            <div class="row g-4">
+                <div class="col-xl-4 col-12">
+                    <div class="section__header mb-xl-0 text-white">
+                        <h2 class="text-white">Festival Schedule</h2>
+                        <p>Dramatically strategize economically sound action items for e-business niches. Quickly re-engineer 24/365 potentialities before.</p>
+                        <p class="mb-0">Holisticly extend sticky partnerships and cross functional markets. Monotonectally.</p>
+                    </div>
+                </div>
+                <div class="col-xl-8 col-12">
+                    <div class="section__wrapper">
+                        <div class="shedule__top">
+                            <div class="shedule__item">
+                                <div class="shedule__inner">
+                                    <div class="shedule__content">
+                                        <div class="shedule__left">
+                                            <div class="shedule__title">
+                                                <h6>Christmas</h6>
+                                                <p>26 June at 6:30 am</p>
+                                            </div>
+                                        </div>
+                                        <div class="shedule__right">
+                                            <div class="shedule__time">
+                                                <ul class="countdown count-down" data-date="Nove 14, 2022 21:14:01">
+                                                    <li class="clock-item">
+                                                        <span class="count-number days">56</span>
+                                                        <p class="count-text">Days</p>
+                                                    </li>
+                                
+                                                    <li class="clock-item">
+                                                        <span class="count-number hours">16</span>
+                                                        <p class="count-text">Hours</p>
+                                                    </li>
+                                                    <li class="clock-item">
+                                                        <span class="count-number minutes">25</span>
+                                                        <p class="count-text">Minutes</p>
+                                                    </li>
+                                
+                                                    <li class="clock-item">
+                                                        <span class="count-number seconds">19</span>
+                                                        <p class="count-text">Seconds</p>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="shedule__item">
+                                <div class="shedule__inner">
+                                    <div class="shedule__content">
+                                        <div class="shedule__left">
+                                            <div class="shedule__title">
+                                                <h6>Holy Cross Day</h6>
+                                                <p>26 June at 6:30 am</p>
+                                            </div>
+                                        </div>
+                                        <div class="shedule__right">
+                                            <div class="shedule__time">
+                                                <ul class="countdown count-down" data-date="Nove 13, 2022 21:14:01">
+                                                    <li class="clock-item">
+                                                        <span class="count-number days">56</span>
+                                                        <p class="count-text">Days</p>
+                                                    </li>
+                                
+                                                    <li class="clock-item">
+                                                        <span class="count-number hours">16</span>
+                                                        <p class="count-text">Hours</p>
+                                                    </li>
+                                                    <li class="clock-item">
+                                                        <span class="count-number minutes">25</span>
+                                                        <p class="count-text">Minutes</p>
+                                                    </li>
+                                
+                                                    <li class="clock-item">
+                                                        <span class="count-number seconds">19</span>
+                                                        <p class="count-text">Seconds</p>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="shedule__item">
+                                <div class="shedule__inner">
+                                    <div class="shedule__content">
+                                        <div class="shedule__left">
+                                            <div class="shedule__title">
+                                                <h6>ST. Patrick Day</h6>
+                                                <p>26 June at 6:30 am</p>
+                                            </div>
+                                        </div>
+                                        <div class="shedule__right">
+                                            <div class="shedule__time">
+                                                <ul class="countdown count-down" data-date="Nove 12, 2022 21:14:01">
+                                                    <li class="clock-item">
+                                                        <span class="count-number days">56</span>
+                                                        <p class="count-text">Days</p>
+                                                    </li>
+                                
+                                                    <li class="clock-item">
+                                                        <span class="count-number hours">16</span>
+                                                        <p class="count-text">Hours</p>
+                                                    </li>
+                                                    <li class="clock-item">
+                                                        <span class="count-number minutes">25</span>
+                                                        <p class="count-text">Minutes</p>
+                                                    </li>
+                                
+                                                    <li class="clock-item">
+                                                        <span class="count-number seconds">19</span>
+                                                        <p class="count-text">Seconds</p>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="shedule__item">
+                                <div class="shedule__inner">
+                                    <div class="shedule__content">
+                                        <div class="shedule__left">
+                                            <div class="shedule__title">
+                                                <h6>Candlemas</h6>
+                                                <p>26 June at 6:30 am</p>
+                                            </div>
+                                        </div>
+                                        <div class="shedule__right">
+                                            <div class="shedule__time">
+                                                <ul class="countdown count-down" data-date="Nove 11, 2022 21:14:01">
+                                                    <li class="clock-item">
+                                                        <span class="count-number days">56</span>
+                                                        <p class="count-text">Days</p>
+                                                    </li>
+                                
+                                                    <li class="clock-item">
+                                                        <span class="count-number hours">16</span>
+                                                        <p class="count-text">Hours</p>
+                                                    </li>
+                                                    <li class="clock-item">
+                                                        <span class="count-number minutes">25</span>
+                                                        <p class="count-text">Minutes</p>
+                                                    </li>
+                                
+                                                    <li class="clock-item">
+                                                        <span class="count-number seconds">19</span>
+                                                        <p class="count-text">Seconds</p>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="shedule__item">
+                                <div class="shedule__inner">
+                                    <div class="shedule__content">
+                                        <div class="shedule__left">
+                                            <div class="shedule__title">
+                                                <h6>Christmas Eve</h6>
+                                                <p>26 June at 6:30 am</p>
+                                            </div>
+                                        </div>
+                                        <div class="shedule__right">
+                                            <div class="shedule__time">
+                                                <ul class="countdown count-down" data-date="Nove 10, 2022 21:14:01">
+                                                    <li class="clock-item">
+                                                        <span class="count-number days">56</span>
+                                                        <p class="count-text">Days</p>
+                                                    </li>
+                                
+                                                    <li class="clock-item">
+                                                        <span class="count-number hours">16</span>
+                                                        <p class="count-text">Hours</p>
+                                                    </li>
+                                                    <li class="clock-item">
+                                                        <span class="count-number minutes">25</span>
+                                                        <p class="count-text">Minutes</p>
+                                                    </li>
+                                
+                                                    <li class="clock-item">
+                                                        <span class="count-number seconds">19</span>
+                                                        <p class="count-text">Seconds</p>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="shedule__item">
+                                <div class="shedule__inner">
+                                    <div class="shedule__content">
+                                        <div class="shedule__left">
+                                            <div class="shedule__title">
+                                                <h6>Ascension Jes</h6>
+                                                <p>26 June at 6:30 am</p>
+                                            </div>
+                                        </div>
+                                        <div class="shedule__right">
+                                            <div class="shedule__time">
+                                                <ul class="countdown count-down" data-date="Nove 09, 2022 21:14:01">
+                                                    <li class="clock-item">
+                                                        <span class="count-number days">56</span>
+                                                        <p class="count-text">Days</p>
+                                                    </li>
+                                
+                                                    <li class="clock-item">
+                                                        <span class="count-number hours">16</span>
+                                                        <p class="count-text">Hours</p>
+                                                    </li>
+                                                    <li class="clock-item">
+                                                        <span class="count-number minutes">25</span>
+                                                        <p class="count-text">Minutes</p>
+                                                    </li>
+                                
+                                                    <li class="clock-item">
+                                                        <span class="count-number seconds">19</span>
+                                                        <p class="count-text">Seconds</p>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="shedule__bottom">
+                            <div class="shedule__sunrise">
+                                <div class="shedule__sunrise-item">
+                                    <div class="shedule__sunrise-inner">
+                                        <div class="shedule__sunrise-thumb">
+                                            <img src="assets/images/shedule/sun.png" alt="event sunrise" />
+                                        </div>
+                                        <div class="shedule__sunrise-content">
+                                            <h3>SUNRISE</h3>
+                                            <p>5.10 AM</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="shedule__sunrise-item">
+                                    <div class="shedule__sunrise-inner">
+                                        <div class="shedule__sunrise-thumb">
+                                            <img src="assets/images/shedule/sun2.png" alt="event sunrise" />
+                                        </div>
+                                        <div class="shedule__sunrise-content">
+                                            <h3>SUNset</h3>
+                                            <p>5.10 AM</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ScheduleSection
