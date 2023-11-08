c4d3345499bbe89319c89dabaeb2f77295d2ad4e
NnebedumEmmanuel <emmanuelnnebedum704@gmail.com>
2023-11-08 09:57:24 +0100

    ContactSection

---
+++ src/components/SectionsPages/ContactSection.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const ContactSection = () => {
+  return (
+   <div class="contact padding--top padding--bottom bg-light">
+        <div class="container">
+            <div class="section__header text-center">
+                <h2>Contact Us</h2>
+                <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
+            </div>
+            <div class="section__wrapper">
+                <div class="contact__form">
+                    <form class="d-flex flex-wrap justify-content-between" action="contact.php" id="contact-form" method="POST">
+                        <input type="text" placeholder="Your Name" id="name" name="name" required="required" />
+                        <input type="text" placeholder="Your Email" id="email" name="email" required="" />
+                        <input class="w-100" type="text" placeholder="Subject" id="subject" name="subject" required="" />
+                        <textarea placeholder="Your Message" rows="8" name="message" id="message" required=""></textarea>
+                        <div class="text-center w-100">
+                            <button type="submit" class="default-btn move-right"><span>SEND NOW</span></button>
+                        </div>
+                    </form>
+                    <p class="form-message"></p> 
+                </div> 
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ContactSection
