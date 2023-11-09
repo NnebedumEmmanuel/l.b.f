9bc1b932ec8e928984295eb2f7cd7eb4b2caf581
NnebedumEmmanuel <emmanuelnnebedum704@gmail.com>
2023-11-09 11:54:21 +0100

    blog

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import PageHeader from '../components/SectionsPages/PageHeader'
 
 const Blog = () => {
   return (
     <div>
+    <PageHeader page="Blog"/>
       
     </div>
   )
--- src/components/SectionsPages/PageHeader.js
+++ src/components/SectionsPages/PageHeader.js
@@ -1,11 +1,29 @@
 import React from 'react'
 
-const PageHeader = () => {
+const PageHeader = (props) => {
   return (
-    <div>
-      
+    <div class="pageheader">
+      <div class="container">
+        <div class="pageheader__area">
+          <div class="pageheader__left">
+            <h3>Blog</h3>
+          </div>
+          <div class="pageheader__right">
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                  <a href="index.html">Home</a>
+                </li>
+                <li class="breadcrumb-item active" aria-current="page">
+                  {props.page}
+                </li>
+              </ol>
+            </nav>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default PageHeader
